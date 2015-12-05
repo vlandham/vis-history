d6434d7141ca57ffe76030abe99269bbfc929014
Jim Vallandingham <vlandham@gmail.com>
2015-12-04 20:01:32 -0800

    Add components to render git stuffs

--- src/app.js
+++ src/app.js
@@ -1,10 +1,105 @@
 require('./index.html');
-require('./style.js')
+require('./style.js');
+require('git-static');
 
-import React from "react";
-import Greeting from "./components/greeting";
+var React = require('react');
+var ReactDOM = require('react-dom');
+var git = require('git-static');
 
-React.render(
-  <Greeting name="World"/>,
-  document.body
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Link = ReactRouter.Link;
+var Navigation = ReactRouter.Navigation;
+var createBrowserHistory = require('history/lib/createBrowserHistory');
+
+
+var Repos = React.createClass({
+  getInitialState : function() {
+    return {
+      repos : {},
+      branches: {}
+    }
+  },
+  componentDidMount : function() {
+    this.loadSamples();
+  },
+  loadSamples : function() {
+    this.setState({
+      repos : require('./repos-config')
+    });
+  },
+  loadBranches : function(repo) {
+    var repo = {};
+    git.getBranches(path, function(branches){
+      this.setState({
+        console.log(branches);
+        branches[repo.path] = branches;
+      });
+    })
+  },
+  renderRepo : function(key){
+    return <RepoIcon key={key} index={key} details={this.state.repos[key]}/>
+  },
+  render: function() {
+    return (
+      <div className="repos-wrap">
+        <ul className="list-of-repos">
+          {Object.keys(this.state.repos).map(this.renderRepo)}
+        </ul>
+      </div>
+    );
+  },
+});
+
+var RepoIcon = React.createClass({
+  onButtonClick : function() {
+    console.log("Going to repo: ", this.props.index);
+    var key = this.props.index;
+    // this.props.addToOrder(key);
+  },
+  render : function() {
+    var details = this.props.details;
+    return (
+        <li key={details.id}><Link to={`/repo/${this.props.index}`}>{details.name}</Link></li>
+    )
+  }
+});
+
+var Histories = React.createClass({
+  componentDidMount : function() {
+  },
+  render : function() {
+    return (
+      <div>
+        <h2>Inventory</h2>
+
+      </div>
+    )
+  }
+})
+
+
+var NotFound = React.createClass({
+  render : function() {
+    return <h1>Not Found!</h1>
+  }
+});
+
+/*
+  Routes
+*/
+
+var routes = (
+  <Router>
+    <Route path="/" component={Repos}/>
+    <Route path="/repo/:repoId" component={Histories}/>
+    <Route path="*" component={NotFound}/>
+  </Router>
+)
+
+
+ReactDOM.render(
+  routes,
+  document.querySelector("#main")
 );
---
+++ src/repos-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  repo1: {
+    path : '~/code/js/buoyant_deploy/',
+    name : 'Buoyant'
+  },
+  repo2: {
+    path : 'code/js/vis/',
+    name : 'Vis'
+  }
+};
